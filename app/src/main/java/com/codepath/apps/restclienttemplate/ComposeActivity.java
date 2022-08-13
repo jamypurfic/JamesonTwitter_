746121adaa82f08904b792cac51732ca5f72a2fc
Jameson Francisque <francisquejamy@gmail.com>
2022-08-13 09:11:52 -0400

    character count added

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,12 +3,51 @@ package com.codepath.apps.restclienttemplate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ComposeActivity extends AppCompatActivity {
+    public static final int MAX_TWEET_LENGH = 280;
+
+    EditText etCompose;
+    Button btnTweet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_compose);
+        etCompose = findViewById(R.id.etCompose);
+        btnTweet = findViewById(R.id.btnTweet);
+        
+        //click listener on the button
+        btnTweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tweetContent = etCompose.getText().toString();
+                if (tweetContent.isEmpty()) {
+                    Toast.makeText(ComposeActivity.this, "Sorry type more", Toast.LENGTH_LONG).show();
+                    return;
+
+                }
+
+                if (tweetContent.length() > 280) {
+                    Toast.makeText(ComposeActivity.this, "Sorry too much", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                //then make api call to twitter to publish the tweet
+                Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        
+
+
+
+
+
     }
 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -44,8 +45,10 @@ public class TimelineActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.compose) {
             //compose item has been selected
-            Toast.makeText(this, "Compose", Toast.LENGTH_LONG).show();
             //Navigate to the compose activity
+            Intent intent = new Intent(this, ComposeActivity.class);
+            startActivity(intent);
+
 
             return true;
         }
